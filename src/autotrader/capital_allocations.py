d385fd663c136c56eb6f2dd6b4f6cd6589d9e2b3
cjhaake1-commits <cjhaake1@gmail.com>
2026-08-17 23:47:31 +0000

    Add reserved IBKR global fourth pillar

--- src/autotrader/capital_allocations.py
+++ src/autotrader/capital_allocations.py
@@ -2,18 +2,23 @@ from __future__ import annotations
 
 from .models import AssetClass
 
-TOTAL_PAPER_CAPITAL = 3000.0
+TOTAL_PAPER_CAPITAL = 4000.0
 PILLAR_CAPITAL = 1000.0
 
 PILLAR_EQUITIES = "alpaca_equities"
 PILLAR_FOREX = "oanda_fx"
 PILLAR_CRYPTO = "alpaca_crypto"
+PILLAR_IBKR_GLOBAL = "ibkr_global"
 
 PILLAR_ALLOCATIONS = {
     PILLAR_EQUITIES: PILLAR_CAPITAL,
     PILLAR_FOREX: PILLAR_CAPITAL,
     PILLAR_CRYPTO: PILLAR_CAPITAL,
+    PILLAR_IBKR_GLOBAL: PILLAR_CAPITAL,
 }
+
+ACTIVE_PILLARS = (PILLAR_EQUITIES, PILLAR_FOREX, PILLAR_CRYPTO)
+RESERVED_PILLARS = (PILLAR_IBKR_GLOBAL,)
 
 
 def pillar_for_asset(asset_class: AssetClass) -> str:
